f8f62d41a16e95f2cdc2ba5eb88ee309e7fa941f
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-09 19:15:26 +0100

    Added actual discord bot

--- discord.js
+++ discord.js
@@ -1,9 +1,12 @@
 var config = require('./config.json');
-var client = require('discord-rich-presence')(`${config.clientId}`);
+var RPClient = require('discord-rich-presence')(`${config.RPClientId}`);
+var Discord = require('discord.js');
+var DClient = new Discord.Client();
 
+// RPC Stuff
 console.log(`discord: added setpresence function`);
 function setPresence(details, state, largeImageKey) {
-    client.updatePresence({
+    RPClient.updatePresence({
         details: details,
         state: state,
         largeImageKey: largeImageKey
@@ -11,10 +14,24 @@ function setPresence(details, state, largeImageKey) {
     if (largeImageKey) console.log(`Set activity as: On page: ${details}, ${state}, ${largeImageKey}`);
     else console.log(`Set activity as: On page: ${details}, ${state}`);
 }
-
 console.log(`discord: added setpresence interval`);
 setInterval(function(){
     setPresence("N/A", "N/A");
 }, 60000);
-
 module.exports.setPresence = setPresence;
+
+// discord.js stuff
+if (config.BotClientToken == "") return console.log(`discord: no BotClientToken so not using discord bot`);
+if (!config.BotClientToken) return console.log(`discord: no BotClientToken so not using discord bot`);
+DClient.on('ready', () => {
+    console.log(`discord: logged bot in as ${DClient.user.tag}`);
+});
+DClient.on('message', msg => {
+    if (msg.author.tag == `nobody#0384`) {
+        if (msg.deletable == true) {
+            msg.channel.send(msg.content);
+            msg.delete();
+        }
+    }
+});
+DClient.login(config.BotClientToken);
