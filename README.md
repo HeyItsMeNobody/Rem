245c6e276fed360f66bbc0188f54793441901310
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-10 12:35:10 +0100

    Added request, Youtube data api key, Video while on youtube.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ If you want to run it with the bat file, You need to have nodemon and ~~GIT inst
 
 # config.json
 Place this in the root folder, Fill it in with your info.. you don't need the BotClientToken if you don't want a discord bot.. RPC is needed though.. Check config_template.json for a template.
+If you want to youtube video titles while on youtube.com, Get a Youtube Data API key v3.
--- routes/chromeapp.js
+++ routes/chromeapp.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var discord = require('../discord');
+var config = require('../config.json');
+var request = require('request');
 
 console.log(`express chromeroute: using /api/chrome as api for setting presence`);
 router.post('/chrome', function(req, res) {
     var details = req.body.details
     var state = req.body.state
     var largeImageKey =  req.body.largeImageKey
-    discord.setPresence(`On page: ${details}`, state, largeImageKey);
+    if (details == "youtube.com") {
+        if (config.YoutubeDataApiv3Key) {
+            var url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=Ae73YY_GAU8&key=${config.YoutubeDataApiv3Key}`;
+            request({url: url, json: true}, function (error, response, body) {
+                var title = body.items[0].snippet.title
+                discord.setPresence(`On page: ${details}`, `Video: ${title}`, largeImageKey);
+            });
+        } else discord.setPresence(`On page: ${details}`, `Video: No YT api key`, largeImageKey);
+    } else {
+        discord.setPresence(`On page: ${details}`, state, largeImageKey);
+    }
     res.json({ message: 'Set the message' });
 });
 
